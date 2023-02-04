b99e19e6d983e4b7841571c4db73a434c4206b47
BRANDON MOONEY <2100664@uad.ac.uk>
2023-02-04 13:36:29 +0000

    Update b4oyah lunch
    wefkpwoiufvpeio

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight;
     private Rigidbody2D rb2d;
 
+    Vector2 _move;
+
     //For checking if we are on the ground
     public bool isGrounded;
     public LayerMask groundLayers;
@@ -19,14 +21,33 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        
+    }
+
     private void FixedUpdate()
     {
         //checks if the player is grounded by cretaing an overlap area checking for the ground layer
-        isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.7f, transform.position.y - 0.7f),
-                                new Vector2(transform.position.x + 0.7f, transform.position.y + 0.7f), groundLayers);
+        isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 1f, transform.position.y - 2.5f),
+                                new Vector2(transform.position.x + 0.5f, transform.position.y + 0.5f), groundLayers);
+        
 
         //basic movement and jumping
-        rb2d.velocity = new Vector2 (Input.GetAxis("Horizontal") * speed , rb2d.velocity.y);
+        rb2d.velocity = (new Vector2(Input.GetAxis("Horizontal") * speed, rb2d.velocity.y));
+
+        if (rb2d.velocity.y > 30)
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 30);
+
+        //flips direction
+
+       
+
+        if ((Input.GetAxis("Horizontal") > 0.01f))
+            transform.localScale = Vector3.one;
+        else if ((Input.GetAxis("Horizontal") < -0.01f))
+            transform.localScale = new Vector3(-1, 1, 1);
+
 
         if (Input.GetButton("Jump") && isGrounded )
         {
@@ -40,9 +61,9 @@ public class PlayerMovement : MonoBehaviour
     }
     private void OnDrawGizmos()
     {
-        //for debugging the 
+        //for debugging if the player is grounded or not
         Gizmos.color = new Color(0, 1, 0, 0.5f);
-        Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y - 0.7f), new Vector2(1, 0.01f));
+        Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y - 2.5f), new Vector2(1, 0.01f));
     }
 
 
