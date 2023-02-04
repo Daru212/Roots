cff49adebbe07773523a8ffb59de2b065b78c8ba
BRANDON MOONEY <2100664@uad.ac.uk>
2023-02-04 17:17:02 +0000

    Big Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
     public float speed;
     public float jumpHeight;
-    private Rigidbody2D rb2d;
-
-    Vector2 _move;
+    bool jumping = false;
 
     //For checking if we are on the ground
     public bool isGrounded;
@@ -17,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         isGrounded = false;
 
     }
@@ -39,26 +43,52 @@ public class PlayerMovement : MonoBehaviour
         if (rb2d.velocity.y > 30)
             rb2d.velocity = new Vector2(rb2d.velocity.x, 30);
 
+
         //flips direction
-
-       
-
         if ((Input.GetAxis("Horizontal") > 0.01f))
             transform.localScale = Vector3.one;
         else if ((Input.GetAxis("Horizontal") < -0.01f))
             transform.localScale = new Vector3(-1, 1, 1);
-
-
-        if (Input.GetButton("Jump") && isGrounded )
+        
+       
+        //jumping
+        if (Input.GetButton("Jump") && isGrounded)
         {
-            //rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
-            rb2d.AddForce (Vector2.up * jumpHeight, ForceMode2D.Impulse);
+           if (jumping == false)
+            {
+                rb2d.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                //anim.SetTrigger("jump");
+                StartCoroutine(jumpdelay());//delays ability to jump
+                StartCoroutine(jumpdelay());
+            }                    
+           
+        }
+        if (rb2d.velocity.y < -0.01f) {
+            anim.SetTrigger("falling");
+        }
+        //if horizontal input detected, set animation running variable to true
+        anim.SetBool("running", (Input.GetAxis("Horizontal") != 0));
+        anim.SetBool("grounded", isGrounded);
+
+
+
+        IEnumerator jumpdelay() 
+        {
+            jumping = true;
+            yield return new WaitForSeconds(0.2f);
+            jumping = false;
         }
 
-
-
+     
 
     }
+
+
+
+
+
+
+
     private void OnDrawGizmos()
     {
         //for debugging if the player is grounded or not
